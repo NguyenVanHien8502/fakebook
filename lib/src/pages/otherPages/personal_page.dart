ee5287522b7bd1e15b0426231bd504c7731f16cd
Nguyen-Ngoc-Linht <ngoclinhtp2002@gmail.com>
2023-10-26 10:21:03 +0700

    fix overflow

--- lib/src/pages/otherPages/personal_page.dart
+++ lib/src/pages/otherPages/personal_page.dart
@@ -7,34 +7,25 @@ class PersonalPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Facebook"),
-          centerTitle: true,
-        ),
-        body: Column(
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.only(left: 24, right: 12, top: 10),
-              child: const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Menu",
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
-                  ),
-                  Text(
-                    "638 bạn bè",
-                    style: TextStyle(fontSize: 10, fontWeight: FontWeight.w500),
-                  ),
-                  Text(
-                    "If you want to",
-                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
-                  ),
-                  MyButton(onTap: null, nameButton: "Thêm vào tin")
-                ],
+      appBar: AppBar(
+        title: const Text("Facebook"),
+        centerTitle: true,
+      ),
+      body: const SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(width: 200),
+              CircleAvatar(
+                backgroundImage:
+                    AssetImage('lib/src/assets/images/fakebook.png'),
+                radius: 60,
               ),
-            ),
-          ],
-        ));
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
